062efd949f941f32661d928df56a2dcdf74a0fef
Gini <64064123+gin4eto7@users.noreply.github.com>
2020-09-26 20:27:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AssociativeArraysLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            SortedDictionary<double, int> count = new SortedDictionary<double, int>();
+
+            foreach (var num in nums)
+            {
+                if(count.ContainsKey(num))
+                {
+                    count[num]++;
+                }
+                else
+                {
+                    count.Add(num, 1);
+                }
+            }
+            foreach (var number in count)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+
+    }
+}
